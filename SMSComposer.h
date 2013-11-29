c0c96a2b3394a0445e0eccf488e0d4978c76f061
Matias Singers <mail@matiassingers.com>
2013-11-29 18:08:08 +0800

    Updated SMSComposer .m/.h to work with Cordova 2.0+(still not functional)

--- SMSComposer.h
+++ SMSComposer.h
@@ -2,12 +2,18 @@
 //  SMSComposer.h
 //
 //  Created by Grant Sanders on 12/25/2010.
-
+//  Modified by Matias Singers on 12/01/2013.
 
 #import <Foundation/Foundation.h>
-#import <PhoneGap/PGPlugin.h>
-@interface SMSComposer : PGPlugin {
-}
+#import <Cordova/CDVPlugin.h>
 
-- (void)showSMSComposer:(NSArray*)arguments withDict:(NSDictionary*)options;
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMessageComposeViewController.h>
+
+NSString *callback;
+
+@interface SMSComposer : CDVPlugin <MFMessageComposeViewControllerDelegate>{}
+@property (retain) NSString *callback;
+
+- (void)showSMSComposer:(CDVInvokedUrlCommand*)command;
 @end
--- SMSComposer.m
+++ SMSComposer.m
@@ -1,85 +1,67 @@
 //
-//  ClipboardPlugin.m
-//  Clipboard plugin for PhoneGap
+//  SMSComposer.h
 //
 //  Created by Grant Sanders on 12/25/2010.
-//
+//  Modified by Matias Singers on 12/01/2013.
 
 #import "SMSComposer.h"
-#import <MessageUI/MessageUI.h>
-#import <MessageUI/MFMessageComposeViewController.h>
 
 @implementation SMSComposer
 
-- (void)showSMSComposer:(NSArray*)arguments withDict:(NSDictionary*)options
+- (CDVPlugin *)initWithWebView:(UIWebView *)theWebView
 {
-	
-	Class messageClass = (NSClassFromString(@"MFMessageComposeViewController"));
-	if (messageClass != nil) {          
-		
-        if (![messageClass canSendText]) {
-			
-			UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Notice" message:@"SMS Text not available."
-														   delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
-			[alert show];
-			[alert release];
-			return;
-        }
-		
-    } else {
-		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Notice" message:@"SMS Text not available."
-													   delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
-		[alert show];
-		[alert release];
-		return;
-	}
-	
-	
-	NSString* body = [options valueForKey:@"body"];
-	NSString* toRecipientsString = [options valueForKey:@"toRecipients"];
-	
-    MFMessageComposeViewController *picker = [[MFMessageComposeViewController alloc] init];
-    picker.messageComposeDelegate = self;
-	
-	if(body != nil)
-		picker.body = [options valueForKey:@"body"];
-	
-	if(toRecipientsString != nil)
-		[picker setRecipients:[ toRecipientsString componentsSeparatedByString:@","]];
-
-    [[ super appViewController ] presentModalViewController:picker animated:YES];
-    [picker release];
-	
+	self = (SMSComposer *)[super initWithWebView:theWebView];
+	return self;
 }
 
-// Dismisses the composition interface when users tap Cancel or Send. Proceeds to update the message field with the result of the operation.
-- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
-{   
-    // Notifies users about errors associated with the interface
-	int webviewResult = 0;
-	
-	switch (result)
-	{
-		case MessageComposeResultCancelled:
-			webviewResult = 0;
-			break;
-		case MessageComposeResultSent:
-			webviewResult = 1;
-			break;
+- (void)showSMSComposer:(CDVInvokedUrlCommand *)command
+{
+	callback = command.callbackId;
+	if (![MFMessageComposeViewController canSendText]) {
+		NSLog(@"SMS composer is not available on this platform.");
+
+		UIAlertView *warningAlert = [[UIAlertView alloc] 
+									initWithTitle:@"Error" 
+									message:@"Your device doesn't support SMS!" 
+									delegate:nil 
+									cancelButtonTitle:@"OK" 
+									otherButtonTitles:nil];
+		[warningAlert show];
+		return;
+	}
+
+	NSArray *recipients = [command.arguments objectAtIndex:0];
+	NSString *body = [command.arguments objectAtIndex:1];
+
+	MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+	messageController.messageComposeDelegate = self;
+	[messageController setRecipients:recipients];
+	[messageController setBody:body];
+
+	 // Present message view controller on screen
+	[self.viewController presentViewController:messageController animated:YES completion:nil];
+}
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult) result
+{
+	CDVPluginResult* pluginResult = nil;
+
+	switch (result) {
 		case MessageComposeResultFailed:
-			webviewResult = 2;
+		{
+			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
 			break;
+		}
+			
 		default:
-			webviewResult = 3;
+		{
+			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 			break;
+		}
 	}
 	
-    [[ super appViewController ] dismissModalViewControllerAnimated:YES];
-	
-	NSString* jsString = [[NSString alloc] initWithFormat:@"window.plugins.smsComposer._didFinishWithResult(%d);",webviewResult];
-	[self writeJavascript:jsString];
-	[jsString release];
-	
+	[self.viewController dismissViewControllerAnimated:YES completion:nil];
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:callback];
 }
 
 @end
